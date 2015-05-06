3e40ef868651bc25ee6df84d5070b66c5dfe7430
unknown <contact@mediaunmasked.com>
2015-05-06 16:09:35 -0400

    gulp sass support

--- gulpfile.js
+++ gulpfile.js
@@ -26,12 +26,12 @@ gulp.task('jekyll', function() {
 });
 
 /* Rebuild Jekyll site & reload page */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function(){
+gulp.task('jekyll-rebuild', ['jekyll'], function(){
 	browserSync.reload();
 });
 
-/* Wait for jekyll, then launch server */
-gulp.task('bs', ['scss', 'jekyll'] function() {
+/* Launch server */
+gulp.task('browser-sync', ['scss', 'jekyll'], function() {
 	browserSync({
 		server: {
 			baseDir: '_site'
@@ -40,43 +40,32 @@ gulp.task('bs', ['scss', 'jekyll'] function() {
 });
 
 gulp.task('scss', function() {
-	gulp.src('assets/style.scss')
+	gulp.src('assets/css/style.scss')
 		.pipe(sass())
 		.pipe(autoprefixer())
 		.pipe(minifycss({keepBreaks: false, keepSpecialComments:true}))
-		.pipe(rename('yolo.css'))
+		.pipe(rename('style.css'))
 		.pipe(gulp.dest('_site/assets/css'))
-		.pipe(browserSync.reload({stream: true}));
-		.pipe(gulp.dest('assets/css'))
+		.pipe(browserSync.reload({stream: true}))
 });
 
-// gulp.task('html', ['jekyll'], function() {
-// 	gulp.src('**/*.html')
-// 		.pipe(htmlmin({collapseWhitespace: true}))
-// 		.pipe(gulp.dest(deploy))
-// 		.pipe(browserSync.reload({stream:true, once: true}));
-// });
+gulp.task('html', ['jekyll'], function() {
+	gulp.src('**/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest(deploy))
+		.pipe(browserSync.reload({stream:true, once: true}));
+});
 
-// gulp.task('js', function () {
-// 	gulp.src('**/*.js')
-// 		.pipe(concat('.js'))
-// 		.pipe(gulp.dest('js'))
-// 		.pipe(rename('.min.js'))
-// 		.pipe(uglify())
-// 		.pipe(gulp.dest('js/min'));
-// });
+gulp.task('js', function () {
+	gulp.src('**/*.js')
+		.pipe(concat('.js'))
+		.pipe(gulp.dest('js'))
+		.pipe(rename('.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('js/min'));
+});
 
-// // gulp.task('jekyll', ['js'], function (gulpCallBack){
-// // 	var spawn = require('child_process').spawn;
-// // 	// After build: cleanup HTML
-// // 	var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
- 
-// // 	jekyll.on('exit', function(code) {
-// // 		gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
-// // 	});
-// // });
- 
-gulp.task('default', ['bs'], function (event) {
+gulp.task('default', ['browser-sync'], function (event) {
 	// gulp.watch([
 	// 	path.join(dist, '*.html'),
 	// 	path.join(dist, '*.md'),
